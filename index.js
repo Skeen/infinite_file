2032e1aad02f3e0c26b7d74522cf671293386f18
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-09-23 19:53:05 +0200

    Added semicolons, very important!

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var cors = require('cors')
+var cors = require('cors');
 
 var app = express();
-app.use(cors())
+app.use(cors());
 
 app.get('/', function(req, res)
 {
