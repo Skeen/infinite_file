0ce52b1800c2f2028593ff4d00a601ec1c0fc127
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-10-13 18:01:11 +0200

    Added hang target!

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.get('/', function(req, res)
     var reply = "Server for loading network connections" + "\n" +
                 "Has two endpoints:" + "\n" + 
                 "* '/undefined/:num_bytes/:throttle?' : Reply with 'num_bytes' of undefined data (at speed of 'trottle' bps)" + "\n" +
-                "* '/random/:num_bytes/:throttle?' : Reply with 'num_bytes' of random data (at speed of 'throttle' bps)";
+                "* '/random/:num_bytes/:throttle?' : Reply with 'num_bytes' of random data (at speed of 'throttle' bps)" + "\n" + 
+                "* '/hang/:timeout' : Reply after 'timeout' milliseconds";
     res.end(reply);
 });
 
@@ -23,6 +24,36 @@ app.get('/random/:num_bytes/:throttle?', function(req, res)
     gen_reply(req, res, true);
 });
 
+app.get('/hang/:timeout', function(req, res)
+{
+    var timeout_str = req.params.timeout;
+    var timeout = parseInt(timeout_str);
+
+    if(timeout_str === undefined)
+    {
+        res.status(400);
+        res.end("Please provide an timeout value!");
+    }
+    else if(isNaN(timeout))
+    {
+        res.status(400);
+        res.end("Please provide an integer timeout value!");
+    }
+    else if(timeout < 0)
+    {
+        res.status(400);
+        res.end("Please provide a non-negative timeout value!");
+    }
+    else
+    {
+        setTimeout(function()
+        {
+            res.status(200);
+            res.end("Timeout (" + timeout + ") done!");
+        }, timeout);
+    }
+});
+
 var gen_reply = function(req, res, random)
 {
     var num_bytes_str = req.params.num_bytes;   
@@ -30,13 +61,19 @@ var gen_reply = function(req, res, random)
 
     var throttle_speed_str = req.params.throttle;
     var throttle_speed = parseInt(throttle_speed_str);
-    if(num_bytes)
+    if(isNaN(num_bytes))
+    {
+        res.status(400);
+        res.end("Please provide an integer byte value!");
+    }
+    else if(num_bytes < 0)
+    {
+        res.status(400);
+        res.end("Please provide a non-negative byte value!");
+    }
+    else 
     {
         send_bytes(res, random, num_bytes, throttle_speed);
-    }
-    else
-    {
-        res.end("Please provide the number of bytes to return!");
     }
 }
 
